18a24a51d6709958dfb5d355bd92a7c3ae3437c6
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-10-28 11:25:46 +0200

    Training Material

---
+++ Ventilsteuerung/ISO-DesignerProjects/Workspace/DefaultPool/Output/DefaultPool.2.c
@@ -0,0 +1,63 @@
+// ISO-Designer ISO 11783   Version 5.6.2.5533 Jetter AG
+// Do not change!
+
+#include "DefaultPool.c.h"
+
+const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset[] = {
+	     1,  // WorkingSet_0_Offset
+	    21,  // Macro_Go_To_DataMask_M1_Offset
+	    34,  // Macro_Go_To_DataMask_M2_Offset
+	    47,  // DataMask_M1_Offset
+	   145,  // DataMask_M2_Offset
+	   165,  // Container_B_Offset
+	   187,  // SoftKeyMask_S1_Offset
+	   217,  // SoftKey_F1_Offset
+	   230,  // SoftKey_F2_Offset
+	   243,  // SoftKey_F3_Offset
+	   256,  // SoftKey_F4_Offset
+	   269,  // SoftKey_F5_Offset
+	   282,  // SoftKey_F6_Offset
+	   295,  // SoftKey_F7_Offset
+	   308,  // SoftKey_F8_Offset
+	   321,  // SoftKey_F9_Offset
+	   334,  // SoftKey_F10_Offset
+	   347,  // SoftKey_F11_Offset
+	   360,  // SoftKey_F12_Offset
+	   373,  // Button_A1_Offset
+	   400,  // Button_A2_Offset
+	   427,  // InputNumber_I1_Offset
+	   465,  // OutputString_WorkingSet_0_Offset
+	   494,  // OutputString_SoftKey_F1_Offset
+	   521,  // OutputString_SoftKey_F2_Offset
+	   548,  // OutputString_SoftKey_F3_Offset
+	   575,  // OutputString_SoftKey_F4_Offset
+	   602,  // OutputString_SoftKey_F5_Offset
+	   629,  // OutputString_SoftKey_F6_Offset
+	   656,  // OutputString_SoftKey_F7_Offset
+	   683,  // OutputString_SoftKey_F8_Offset
+	   710,  // OutputString_SoftKey_F9_Offset
+	   737,  // OutputString_SoftKey_F10_Offset
+	   765,  // OutputString_SoftKey_F11_Offset
+	   793,  // OutputString_SoftKey_F12_Offset
+	   821,  // OutputString_DataMask_M1_Offset
+	   849,  // OutputString_DataMask_M2_Offset
+	   877,  // OutputString_Go_To_DataMask_M1_Offset
+	   911,  // OutputString_Go_To_DataMask_M2_Offset
+	   945,  // OutputString_Button_A1_Offset
+	   971,  // OutputString_Button_A2_Offset
+	   997,  // OutputString_OutputNumber_N1_Offset
+	  1029,  // OutputString_OutputNumber_N2_Offset
+	  1061,  // OutputString_InputNumber_I1_Offset
+	  1092,  // OutputNumber_N1_Offset
+	  1121,  // OutputNumber_N2_Offset
+	  1150,  // Line_W_Offset
+	  1161,  // Line_S_Offset
+	  1172,  // Logos_icon_Offset
+	  1701,  // FontAttributes_6x8_Offset
+	  1709,  // FontAttributes_8x12_Offset
+	  1717,  // FontAttributes_12x16_Offset
+	  1725,  // FontAttributes_23003_Offset
+	  1733,  // LineAttributes_black_Offset
+	  1741,  // ObjectPointer_P1_Offset
+	  1746,  // AuxFunction2_X1_Offset
+};  // isoOP_DefaultPool_Offset
